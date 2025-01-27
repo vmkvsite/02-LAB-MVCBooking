f1a9e06773b2eebaa4013c1470b81bb7bb36a525
vmkvsite <vikronja@vsite.hr>
2025-01-27 21:20:09 +0100

    .

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Collections.Generic;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                booking.Id = bookings.Count + 1;
+                bookings.Add(booking);
+                return RedirectToAction("Index");
+            }
+            return View("Create", booking);
+        }
+    }
+}
---
+++ MVCBooking/Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCBooking.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
